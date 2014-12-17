5792af041a4ed066d8cbcea8719a975138825202
BadPointer0@gmail.com <BadPointer0@gmail.com@495bb48d-63ac-580f-82bd-0d83f90b68b7>
2014-12-17 09:47:01 +0000

    *Updated to webp from 0.4.0 to 0.4.2 *Updated libtorrent from 1.0.2 to 1.0.3 *Fixed compiler errors with latest MinGW *Fixed DHT *Added torrent logging

--- external/make_libtorrent.bat
+++ external/make_libtorrent.bat
@@ -23,42 +23,53 @@ if /I not exist "%BOOST_ROOT%\boost.png" (echo ERROR: BOOST not found in %BOOST_
 echo BOOST:      %BOOST_ROOT%
 echo.
 
+rem Check for webp
+if /I not exist ..\webp\mingw\msys\1.0\home\libwebp-0.4.2.tar.gz  (echo ERROR: libwebp-0.4.2.tar.gz not found in webp\mingw\msys\1.0\home & goto EOF)
+
 rem prepare for webp
-if /I exist %GCC_PATH%\msys\1.0\etc\fstab (echo Skipping prepwebp & goto skipprepwebp)
+if /I exist %GCC_PATH%\msys\1.0\home\libwebp-0.4.2.tar.gz (echo Skipping prepwebp & goto skipprepwebp)
 xcopy ..\webp\mingw %GCC_PATH% /E /I /Y
 echo %GCC_PATH% /mingw > %GCC_PATH%\msys\1.0\etc\fstab
 :skipprepwebp
 
 rem Build bjam.exe
+if /I not exist %GCC_PATH%\include\wspiapi.h (copy ..\asio\wspiapi.h %GCC_PATH%\include\wspiapi.h /Y)
+if /I not exist "%BOOST_ROOT%\boost\asio\detail\socket_types.hpp" (copy ..\asio\socket_types.hpp "%BOOST_ROOT%\boost\asio\detail\socket_types.hpp" /Y)
 if /I exist "%BOOST_ROOT%\bjam.exe" (echo Skipping Build bjam.exe & goto skipbuildbjam)
-xcopy ..\asio\wspiapi.h %GCC_PATH%\include\wspiapi.h /Y
-xcopy ..\asio\socket_types.hpp "%BOOST_ROOT%\boost\asio\detail\socket_types.hpp" /Y
 cd "%BOOST_ROOT%"
 call bootstrap.bat mingw
 :skipbuildbjam
 
 rem Build libtorrent.a
 cd "%LIBTORRENT_PATH%"
-if /I exist "%LIBTORRENT_PATH%\bin\gcc-mingw-%GCC_VERSION%\myrelease\exception-handling-off\libtorrent.a" (echo Skipping Build libtorrent & goto skipbuildlibtorrent)
+if /I exist "%LIBTORRENT_PATH%\bin\gcc-mngw-%GCC_VERSION%\myrls\excpt-hndl-off\libtorrent.a" (echo Skipping Build libtorrent & goto skipbuildlibtorrent)
 cd examples
 copy "%LIBTORRENT_PATH%\..\Jamfile_fixed" "%LIBTORRENT_PATH%\examples\Jamfile" /Y
 copy "%LIBTORRENT_PATH%\..\lt_trackers.cpp" "%LIBTORRENT_PATH%\src\lt_trackers.cpp" /Y
-bjam client_test -j%NUMBER_OF_PROCESSORS% toolset=gcc myrelease exception-handling=off "-sBUILD=<define>BOOST_NO_EXCEPTIONS" "-sBUILD=<define>BOOST_EXCEPTION_DISABLE" "cxxflags=-fexpensive-optimizations -fomit-frame-pointer -D IPV6_TCLASS=30"
+bjam --abbreviate-paths client_test -j%NUMBER_OF_PROCESSORS% toolset=gcc myrelease exception-handling=off "-sBUILD=<define>BOOST_NO_EXCEPTIONS" "-sBUILD=<define>BOOST_EXCEPTION_DISABLE" "cxxflags=-fexpensive-optimizations -fomit-frame-pointer -D IPV6_TCLASS=30"
 cd ..
 :skipbuildlibtorrent
 
 rem Copy libtorrent.a
 if /I exist %GCC_PATH%\lib\libtorrent.a (echo Skipping copy libtorrent.a & goto skipcopylibtorrent)
-xcopy bin\gcc-mingw-%GCC_VERSION%\myrelease\exception-handling-off\libtorrent.a %GCC_PATH%\lib /Y
-xcopy include %GCC_PATH%\include /E /I /Y
+copy bin\gcc-mngw-%GCC_VERSION%\myrls\excpt-hndl-off\libtorrent.a %GCC_PATH%\lib /Y
 :skipcopylibtorrent
 
-rem Install BOOST
-if /I exist "%BOOST_ROOT%\boost" (echo Skipping BOOST[include\boost] & goto skipboost1)
+rem Copy libtorrent.a
+if /I exist %GCC_PATH%\include\libtorrent (echo Skipping copy %GCC_PATH%\include\libtorrent & goto skipcopylibtorrentinc)
+xcopy include %GCC_PATH%\include /E /I /Y
+:skipcopylibtorrentinc
+
+rem Copy %GCC_PATH%\include\boost
+if /I exist %GCC_PATH%\include\boost (echo Skipping BOOST[include\boost] & goto skipboost1)
+echo Copying %GCC_PATH%\include\boost
 xcopy "%BOOST_ROOT%\boost" %GCC_PATH%\include\boost /E /I /Y
 :skipboost1
+
+rem Copy libboost_system.a
 if /I exist %GCC_PATH%\lib\libboost_system.a (echo Skipping BOOST[libboost_system.a] & goto skipboost2)
-xcopy "%BOOST_ROOT%\bin.v2\libs\system\build\gcc-mingw-%GCC_VERSION%\myrelease\exception-handling-off\*.a" %GCC_PATH%\lib\libboost_system.a /Y
+echo Copying libboost_system.a
+copy "%BOOST_ROOT%\bin.v2\libs\system\build\gcc-mngw-%GCC_VERSION%\myrls\excpt-hndl-off\*.a" %GCC_PATH%\lib\libboost_system.a
 :skipboost2
 
 echo.
